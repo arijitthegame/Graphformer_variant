a9483fa702681205e6389dfd43b26d25c3ff2d0d
Arijit Sehanobish <arijitthegame@gmail.com>
2022-04-26 21:34:41 -0400

    add some activations

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn.functional as F 
+
+def get_activation_fn(activation: str) -> Callable:
+    """Returns the activation function corresponding to `activation`"""
+    if activation == "relu":
+        return F.relu
+
+    elif activation == "gelu":
+        return torch.nn.GELU
+      
+    elif activation == "tanh":
+        return torch.tanh
+    elif activation == "linear":
+        return lambda x: x
+    elif activation == "swish":
+        return torch.nn.SiLU
+    else:
+        raise RuntimeError("--activation-fn {} not supported".format(activation))
